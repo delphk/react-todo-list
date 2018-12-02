8b96edb3318e77dcd121dab81fc00bb4bf0ff111
delphinekoh <delphinekoh@hotmail.com>
2018-12-02 14:06:32 +0800

    remove comments

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,61 +1,67 @@
-import React, { Component } from 'react';
-import TodoItem from './TodoItem';
-import TodoCreationBar from './TodoCreationBar';
-import TodoFilterBar from './TodoFilterBar'
-import PropTypes from 'prop-types'
-
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+import TodoCreationBar from "./TodoCreationBar";
+import TodoFilterBar from "./TodoFilterBar";
+import PropTypes from "prop-types";
 
 class TodoList extends Component {
-    constructor(props) {
-        super(props);
-        this.state ={
-            todosList: this.props.todos,
-            filterValue: ""
-        }
-    }
-    
-    toggleCompletedTask = (index) => {
-        let todosList = [...this.state.todosList]
-        todosList[index].isCompleted = !todosList[index].isCompleted
-        this.setState({todosList})
-        // item.isCompleted = !item.isCompleted
-        // console.log(this.state.todosList)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      todosList: this.props.todos,
+      filterValue: ""
+    };
+  }
 
-    addNewTask = (task) => {
-        let todosList = [...this.state.todosList]
-        todosList.push({name: task, isCompleted: false})
-        this.setState({todosList})
-    }
+  toggleCompletedTask = index => {
+    let todosList = [...this.state.todosList];
+    todosList[index].isCompleted = !todosList[index].isCompleted;
+    this.setState({ todosList });
+  };
 
-    handleFilter = (input) => {
-        this.setState({filterValue: input})
-    }
+  addNewTask = task => {
+    let todosList = [...this.state.todosList];
+    todosList.push({ name: task, isCompleted: false });
+    this.setState({ todosList });
+  };
 
-    handleDelete = (index) => {
-        let todosList = [...this.state.todosList]
-        todosList.splice(index, 1)
-        this.setState({todosList})
-    }
-    
+  handleFilter = input => {
+    this.setState({ filterValue: input });
+  };
 
+  handleDelete = index => {
+    let todosList = [...this.state.todosList];
+    todosList.splice(index, 1);
+    this.setState({ todosList });
+  };
 
-    render() {
-        let filteredItems = this.state.todosList.filter(list => list.name.toLowerCase().includes(this.state.filterValue.toLowerCase()))
+  render() {
+    let filteredItems = this.state.todosList.filter(list =>
+      list.name.toLowerCase().includes(this.state.filterValue.toLowerCase())
+    );
 
-        return (
-            <div>
-                <TodoCreationBar onAdd={this.addNewTask}/>
-                <TodoFilterBar onFilter={this.handleFilter}/>
-               {filteredItems.length > 0 && filteredItems.map((todo, index) => <TodoItem key={index} index={index} todo= {todo} onDone={this.toggleCompletedTask} handleDelete={this.handleDelete}/>)}
-               {filteredItems.length === 0 && <p>Task not found!</p>}
-            </div>
-        );
-    }
+    return (
+      <div>
+        <TodoCreationBar onAdd={this.addNewTask} />
+        <TodoFilterBar onFilter={this.handleFilter} />
+        {filteredItems.length > 0 &&
+          filteredItems.map((todo, index) => (
+            <TodoItem
+              key={index}
+              index={index}
+              todo={todo}
+              onDone={this.toggleCompletedTask}
+              handleDelete={this.handleDelete}
+            />
+          ))}
+        {filteredItems.length === 0 && <p>Task not found!</p>}
+      </div>
+    );
+  }
 }
 
 TodoList.propTypes = {
-    todos: PropTypes.array.isRequired
-}
+  todos: PropTypes.array.isRequired
+};
 
 export default TodoList;
